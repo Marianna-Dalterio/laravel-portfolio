7230ae498d278dc388b88fcc1b77ecf92fe1f35d
Marianna-Dalterio <mariannadalterio00@gmail.com>
2026-02-15 01:21:49 +0100

    Add Dashboard controller and route for admin index

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\Dashboard;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,16 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+
+Route::middleware(["auth", "verified"])
+    ->name("admin.")
+    ->prefix("admin")
+    ->group(function () {
+
+        Route::get("/", [Dashboard::class, "index"])
+            ->name("index");
+    });
+
+
+
+require __DIR__ . '/auth.php';
