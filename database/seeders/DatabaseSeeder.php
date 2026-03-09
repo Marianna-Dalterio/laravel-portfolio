73eb1c08805dc2081756a87ab54c4ac4d312ea53
Marianna-Dalterio <mariannadalterio00@gmail.com>
2026-03-09 18:17:27 +0100

    Update seeder classes to ensure Technology records are created before Projects and associate random technologies with projects

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TypeTableSeeder::class,
-            ProjectsTableSeeder::class,
-            TechnologyTableSeeder::class,
+            TechnologyTableSeeder::class, //ora esistono le tecnologie
+            ProjectsTableSeeder::class, //possiamo associarle nel db 
         ]);
     }
 }
--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Technology;
 use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,6 +20,8 @@ class ProjectsTableSeeder extends Seeder
 
         //recupero i Types 
         $types = Type::all();
+        // 1. Recupero tutti gli ID delle tecnologie già creati con il TechnologyTableSeeder
+        $technologyIds = Technology::all()->pluck('id');
 
         for ($i = 0; $i < 10; $i++) {
             $newProject = new Project();
@@ -33,6 +36,13 @@ class ProjectsTableSeeder extends Seeder
             $newProject->overview = $faker->paragraph();
 
             $newProject->save();
+
+            // 2. BONUS: Collego da 1 a 4 tecnologie a caso
+            // Prendo un array di ID casuali tra quelli disponibili
+            $randomTechs = $faker->randomElements($technologyIds, rand(1, 4));
+
+            // Uso il metodo attach() sulla relazione
+            $newProject->technologies()->attach($randomTechs);
         }
     }
 }
